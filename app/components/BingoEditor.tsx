5c63c529923bc18090672d7bcd4c5a4512b27bdc
Rahul <37151383+rahulr8@users.noreply.github.com>
2024-10-14 13:21:40 -0700

    fix

--- app/components/BingoEditor.tsx
+++ app/components/BingoEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CollaborativeBingoEditor } from "./CollaborativeBingoEditor";
+import CollaborativeBingoEditor from "./CollaborativeBingoEditor";
 
 export function BingoEditor({ code }: { code: string }) {
   return <CollaborativeBingoEditor code={code} />;
